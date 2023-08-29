c016d915e6f7703a160f175590352d849ba3c626
LALITHESH SAI BALU MOPIDEVI <109448681+Lalithesh-471@users.noreply.github.com>
2023-08-29 19:31:29 +0530

    Delete code02.js

--- code02.js
+++
@@ -1,8 +0,0 @@
-function getStringValue() {
-    return "Blackbucks";
-}
-
-// Calling the function and storing the returned value
-var result = getStringValue();
-
-console.log(result); // Output: This is a string value.
