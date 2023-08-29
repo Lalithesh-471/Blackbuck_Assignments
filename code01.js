4def6247275d5a12a093ca283676621ea9da31bc
LALITHESH SAI BALU MOPIDEVI <109448681+Lalithesh-471@users.noreply.github.com>
2023-08-29 19:31:15 +0530

    Delete code01.js

--- code01.js
+++
@@ -1,8 +0,0 @@
-function printParameters(param1, param2, param3) {
-    console.log("Parameter 1:", param1);
-    console.log("Parameter 2:", param2);
-    console.log("Parameter 3:", param3);
-}
-
-// Calling the function with specific values
-printParameters("Lalithesh", 471, true);
